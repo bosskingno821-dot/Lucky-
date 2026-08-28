5a4ca6a0a111b3336e07fe51de060848042ab95d
bosskingno821-dot <bosskingno821@gmail.com>
2026-08-28 15:06:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import jobRoutes from "./routes/jobRoutes.js";
+import jobRoutes from "./jobRoutes.js";
 
 dotenv.config();
 
